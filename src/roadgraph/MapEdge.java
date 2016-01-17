9f8685375997728fe574cb475100762d6466972a
Benson Malonzo <benson782@hotmail.com>
2016-01-17 01:28:47 -0800

    Week2
    Add comments

--- src/roadgraph/MapEdge.java
+++ src/roadgraph/MapEdge.java
@@ -5,15 +5,25 @@ import geography.GeographicPoint;
 /**
  * @author Benson
  *
+ * A class the represents a map node's edge.
  */
 
 public class MapEdge {
+	// Initialize variables
 	private GeographicPoint start;
 	private GeographicPoint end;
 	private String roadName;
 	private String roadType;
 	private double length;
 	
+	/**
+	 * Constructor
+	 * @param start  The start of the edge
+	 * @param end The end of the edge
+	 * @param roadName The road name
+	 * @param roadType The road type
+	 * @param length The length of the edge
+	 */
 	public MapEdge(GeographicPoint start, 
 			GeographicPoint end,
 			String roadName,
@@ -26,22 +36,42 @@ public class MapEdge {
 		this.length = length;
 	}
 
+	/**
+	 * Get the start of the edge
+	 * @return Start of the edge
+	 */
 	public GeographicPoint getStart() {
 		return start;
 	}
 
+	/**
+	 * Get the end of the edge
+	 * @return End of the edge
+	 */
 	public GeographicPoint getEnd() {
 		return end;
 	}
 
+	/**
+	 * Get the road name of the edge
+	 * @return The road name
+	 */
 	public String getRoadName() {
 		return roadName;
 	}
 
+	/**
+	 * Get the road type of the edge
+	 * @return The road type
+	 */
 	public String getRoadType() {
 		return roadType;
 	}
 
+	/** 
+	 * Get the length of the edge
+	 * @return The length
+	 */
 	public double getLength() {
 		return length;
 	}
